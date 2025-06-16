792f4cb72e0930e4dfd2eb402cd0605d38be8a66
AugustoNeri <avatar.neri96@gmail.com>
2025-06-16 14:11:05 -0600

    programa modificado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,7 +12,7 @@ export const routes: Routes = [
   { path: 'pokemon', component: Datacomponent },
   
   
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '/home' }
 ];
 
 @NgModule({
